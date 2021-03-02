51b2bdb6a93e4109c981663945b20ab58cd867ba
Zornitsa Dineva <zornitsa_angelova@yahoo.com>
2021-03-02 15:22:29 +0200

    bg

---
+++ resources/lang/bg/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Паролата трябва да бъде поне шест символа ида съответства на потвърждението.',
+    'reset' => 'Паролата ви е нулирана!',
+    'sent' => 'Ще получите на пощата линк за промяна на паролата.',
+    'token' => 'Този токан е невалиден',
+    'user' => "Не може да намерим потребител с такъв имейл",
+
+];
