569221060971daa0d4994d3137d17e0aad61d917
Zornitsa Dineva <zornitsa_angelova@yahoo.com>
2021-03-04 14:42:46 +0200

    bg_category and subcategory

--- database/seeds/divisions.sql
+++ database/seeds/divisions.sql
@@ -1,9 +1,28 @@
 INSERT INTO `divisions` (`division_id`, `division_weight`, `division_title_en`, `division_title_bg`, `division_icon`, `created_at`, `updated_at`) VALUES
-(1, 4, 'Barisal', 'বরিশাল', 'icofont icofont-island-alt', '2018-03-25 02:18:27', '2018-03-25 02:25:06'),
-(2, 1, 'Chittagong', 'চট্টগ্রাম', 'icofont icofont-hill-side', '2018-03-25 02:17:59', '2018-03-25 02:24:21'),
-(3, 0, 'Dhaka', 'ঢাকা', 'icofont icofont-building-alt', '2018-03-25 02:17:09', '2018-03-25 02:24:11'),
-(4, 3, 'Khulna', 'খুলনা', 'icofont icofont-ship-alt', '2018-03-25 02:18:16', '2018-03-25 02:24:52'),
-(5, 5, 'Rajshahi', 'রাজশাহী', 'icofont icofont-farmer', '2018-03-25 02:18:37', '2018-03-25 02:25:19'),
-(6, 6, 'Rangpur', 'রংপুর', 'icofont icofont-hill-side', '2018-03-25 02:18:49', '2018-03-25 02:33:14'),
-(7, 2, 'Sylhet', 'সিলেট', 'icofont icofont-tea', '2018-03-25 02:18:06', '2018-03-25 02:24:40'),
-(8, 7, 'Mymenshingh', 'ময়মনসিংহ', 'icofont icofont-direction-sign', '2018-03-25 02:19:17', '2018-03-25 02:33:42');
+(1, 4, 'Sliven', 'Сливен', 'icofont icofont-island-alt', '2018-03-25 02:18:27', '2018-03-25 02:25:06'),
+(2, 1, 'Haskovo', 'Хасково', 'icofont icofont-hill-side', '2018-03-25 02:17:59', '2018-03-25 02:24:21'),
+(3, 0, 'Stara Zagora', 'Стара Загора', 'icofont icofont-building-alt', '2018-03-25 02:17:09', '2018-03-25 02:24:11'),
+(4, 3, 'Pazardik', 'Пазарджик', 'icofont icofont-ship-alt', '2018-03-25 02:18:16', '2018-03-25 02:24:52'),
+(5, 5, 'Plovdiv', 'Пловдив', 'icofont icofont-farmer', '2018-03-25 02:18:37', '2018-03-25 02:25:19'),
+(6, 6, 'Smolqn', 'Смолян', 'icofont icofont-hill-side', '2018-03-25 02:18:49', '2018-03-25 02:33:14'),
+(7, 2, 'Kardjaly', 'Кърджали', 'icofont icofont-tea', '2018-03-25 02:18:06', '2018-03-25 02:24:40'),
+(8, 7, 'Sofiq sity', 'град София', 'icofont icofont-direction-sign', '2018-03-25 02:19:17', '2018-03-25 02:33:42');
+(9,8,'Pernik','Перник')
+(10,9,'Gabrovo','Габрово')
+(11,10,'Lovech','Ловеч')
+(12,11,'Pleven','Плевен')
+(13,12, 'Veliko Tyrnovo','Велико Търново')
+(14,13, 'Montana','Монтана')
+(15,14, 'Vrasa','Враца')
+(16,15, 'Kyustendil','Кюстендил')
+(17,16, 'Vidin','Видин')
+(18,17, 'Burgas','Бургас')
+(19,18, 'Yambol','Ямбол')
+(20,19, 'Razgrad','Разград')
+(21,20, 'Targovishte','Търговище')
+(22,21, 'Shumen','Шумен')
+(23,22, 'Dobrich','Добрич')
+(24,21, 'Varna','Варна')
+(25,24, 'Silistra','Силистра')
+(26,25, 'Ruse','Русе')
+(27,26, 'Blagoewgrad','Благоевград')
